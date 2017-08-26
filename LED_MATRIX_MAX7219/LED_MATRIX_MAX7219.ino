4fc82543b263bbb12a6e74b06291cb9ca1a89a7e
Karem Elzftawy <karem.elzftawy@gmail.com>
2017-08-26 15:29:14 +0200

    Add files via upload

---
+++ LED_MATRIX_MAX7219/LED_MATRIX_MAX7219.ino
@@ -0,0 +1,67 @@
+//We always have to include the library
+#include "LedControl.h"
+
+/*
+ 
+ Now we need a LedControl to work with.
+ ***** These pin numbers will probably not work with your hardware *****
+ pin 12 is connected to the DataIn 
+ pin 11 is connected to the CLK 
+ pin 10 is connected to LOAD 
+ We have only a single MAX72XX.
+ */
+LedControl lc=LedControl(12,11,10,1);
+
+/* we always wait a bit between updates of the display */
+unsigned long delaytime=100;
+byte a[]={B00000000,
+B01000010,
+B11100111,
+B11111111,
+B11111111,
+B01111110,
+B00111100,
+B00011000};
+byte b[]={B00000000,
+B00000000,
+B00100100,
+B01111110,
+B01111110,
+B00111100,
+B00011000,
+B00000000};
+void setup() {
+  /*
+   The MAX72XX is in power-saving mode on startup,
+   we have to do a wakeup call
+   */
+  lc.shutdown(0,false);
+  /* Set the brightness to a medium values */
+  lc.setIntensity(0,8);
+  /* and clear the display */
+  lc.clearDisplay(0);
+   
+}
+void loop(){
+  lc.setRow(0,0,a[0]);
+  lc.setRow(0,1,a[1]);
+  lc.setRow(0,2,a[2]);
+  lc.setRow(0,3,a[3]);
+  lc.setRow(0,4,a[4]);
+  lc.setRow(0,5,a[5]);
+  lc.setRow(0,6,a[6]);
+  lc.setRow(0,7,a[7]);
+  delay(1000);
+  
+  lc.setRow(0,0,b[0]);
+  lc.setRow(0,1,b[1]);
+  lc.setRow(0,2,b[2]);
+  lc.setRow(0,3,b[3]);
+  lc.setRow(0,4,b[4]);
+  lc.setRow(0,5,b[5]);
+  lc.setRow(0,6,b[6]);
+  lc.setRow(0,7,b[7]);
+delay(1000);
+
+}
+
